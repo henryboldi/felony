a9ced8bf64032e40c50874e33de050c070261d7e
case <case@casesandberg.com>
2016-01-10 22:54:56 -0600

    syntax tweak

--- devServer.js
+++ devServer.js
@@ -2,7 +2,7 @@ require('babel-register');
 var proc = require('child_process');
 
 // Emit File Events For JSPM to Hot Reload
-require('chokidar-socket-emitter')({app: 1111});
+require('chokidar-socket-emitter')({ app: 1111 });
 
 // Start Electron
 var child = proc.exec('electron .');
