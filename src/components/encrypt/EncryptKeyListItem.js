c7ff1ef3550c46c3ee97740c97ca1d47a3914217
Henry Boldizsar <henrymb67@gmail.com>
2016-01-29 13:50:14 -0800

    Add encrypt key list item styles

--- src/components/encrypt/EncryptKeyListItem.js
+++ src/components/encrypt/EncryptKeyListItem.js
@@ -3,11 +3,24 @@
 import React, { Component } from 'react';
 import ReactCSS from 'reactcss';
 
+import { User } from '../common/index';
+
+import colors from '../../styles/variables/colors';
+import { spacing, sizing } from '../../styles/variables/utils';
+
 class EncryptKeyListItem extends Component {
   classes() {
     return {
       'default': {
-
+        user: {
+          padding: `${spacing.m}px ${spacing.m}px ${spacing.m}px`,
+          display: 'flex',
+        },
+        spaceLine: {
+          borderBottom: `solid 1px ${colors.offBgLight}`,
+          margin: `0px ${spacing.m}px 0px ${spacing.m + sizing.avatar + spacing.s}px`,
+          item: 1,
+        },
       },
     };
   }
