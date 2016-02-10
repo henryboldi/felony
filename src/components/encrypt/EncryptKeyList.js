67332d38ebec4741803e48f5ea2063e63779294f
Henry Boldizsar <henrymb67@gmail.com>
2016-02-10 00:32:48 -0600

    Add encrypt key list item

--- src/components/encrypt/EncryptKeyList.js
+++ src/components/encrypt/EncryptKeyList.js
@@ -1,9 +1,9 @@
-'use strict';
+'use strict'
 
-import React, { Component } from 'react';
-import ReactCSS from 'reactcss';
+import React, { Component } from 'react'
+import ReactCSS from 'reactcss'
 
-import EncryptKeyListItem from './EncryptKeyListItem';
+import EncryptKeyListItem from './EncryptKeyListItem'
 
 class EncryptKeyList extends Component {
   classes() {
@@ -11,54 +11,27 @@ class EncryptKeyList extends Component {
       'default': {
 
       },
-    };
+    }
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.props.onTestAdd('Case')
+    }, 1000)
   }
 
   render() {
-    const keys = [
-      {
-        id: 1,
-        name: 'Medibuds',
-      },
-      {
-        id: 2,
-        name: 'TripWithScience',
-      },
-      {
-        id: 3,
-        name: 'HumboltFarms',
-      },
-      {
-        id: 4,
-        name: 'HouseOfSpirits',
-      },
-      {
-        id: 5,
-        name: 'HouseOfSpirits',
-      },
-      {
-        id: 6,
-        name: 'HouseOfSpirits',
-      },
-      {
-        id: 7,
-        name: 'HouseOfSpirits',
-      },
-      {
-        id: 8,
-        name: 'HouseOfSpirits',
-      },
-    ];
-
-    return <div>
-      { keys.map((key, j) => {
-        return <EncryptKeyListItem
-          key={ key.id } // TODO: come up with a better variable name than key
-          name={ key.name }
-        />;
-      }) }
-    </div>;
+    return (
+        <div>
+        { this.props.keys && this.props.keys.map((key, j) => {
+          return <EncryptKeyListItem ref="input"
+            key={ key.id } // TODO: come up with a better variable name than key
+            name={ key.name }
+          />
+        }) }
+        </div>
+    )
   }
 }
 
-export default ReactCSS(EncryptKeyList);
+export default ReactCSS(EncryptKeyList)
