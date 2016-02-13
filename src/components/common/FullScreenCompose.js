c8aa646c073d06443c9f584c2d9ddaac92540700
case <case@casesandberg.com>
2016-02-13 17:02:19 -0600

    Add `FullScreenCompose`

---
+++ src/components/common/FullScreenCompose.js
@@ -0,0 +1,67 @@
+'use strict';
+
+import React, { Component } from 'react';
+import ReactCSS from 'reactcss';
+
+class FullScreenTextArea extends Component {
+  static defaultProps = {
+    position: 'fixed',
+  };
+
+  classes() {
+    return {
+      'default': {
+        wrap: {
+          position: this.props.position,
+          top: '0',
+          right: '0',
+          bottom: '0',
+          left: '0',
+          zIndex: '10',
+          background: '#fff',
+          display: 'flex',
+          flexDirection: 'column',
+          background: '#eee',
+        },
+        text: {
+          flex: '1',
+          position: 'relative',
+        },
+        textarea: {
+          Absolute: '10px 10px 10px 10px',
+          padding: '3%',
+          border: 'none',
+          outline: 'none',
+          width: '87%',
+        },
+        actions: {
+          marginTop: '-10px',
+          height: '40px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'flex-end',
+        },
+      },
+      'actions-false': {
+        actions: {
+          display: 'none',
+        },
+      },
+    };
+  }
+
+  render() {
+    return (
+      <div is="wrap">
+        <div is="text">
+          <textarea is="textarea" placeholder="Write a Message" onKeyDown={ this.props.handleKeyDown } />
+        </div>
+        <div is="actions">
+          <a href="#">Encrypt</a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ReactCSS(FullScreenTextArea);
--- src/components/encrypt/EncryptCompose.js
+++ src/components/encrypt/EncryptCompose.js
@@ -3,7 +3,13 @@
 import React, { Component } from 'react';
 import ReactCSS from 'reactcss';
 
+import { FullScreenCompose } from '../common/index';
+
 class EncryptCompose extends Component {
+  state = {
+    'full-screen-compose': false,
+  };
+
   classes() {
     return {
       'default': {
@@ -13,7 +19,9 @@ class EncryptCompose extends Component {
           right: '0',
           left: '0',
           boxShadow: '0 0 14px rgba(0,0,0,0.08), 0 0 4px rgba(0,0,0,0.10)',
-          display: 'none',
+          height: '50px',
+
+          // display: 'none',
         },
         textarea: {
           width: '94%',
@@ -22,19 +30,42 @@ class EncryptCompose extends Component {
           display: 'block',
           outline: 'none',
         },
+
+        Compose: {
+          actions: 'false',
+          position: 'absolute',
+        },
       },
       'display': {
         compose: {
           display: 'block',
         },
       },
+      'full-screen-compose': {
+        Compose: {
+          position: 'fixed',
+          actions: 'true',
+        },
+      },
     };
   }
 
+  activations() {
+    return {
+      'full-screen-compose': this.state['full-screen-compose'],
+    };
+  }
+
+  handleExpandCompose = () => {
+    this.setState({
+      'full-screen-compose': true,
+    });
+  };
+
   render() {
     return (
-      <div is="compose">
-        <textarea is="textarea" placeholder="Write a Message" />
+      <div is="compose" onClick={ this.handleExpandCompose }>
+        <FullScreenCompose is="Compose" />
       </div>
     );
   }
