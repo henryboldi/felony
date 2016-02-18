8c3c936117c714916df85585e7e8278ef7bdba94
Henry Boldizsar <henrymb67@gmail.com>
2016-02-17 22:43:35 -0600

    Improve floating button item styles

--- src/components/floating-button/FloatingButtonItem.js
+++ src/components/floating-button/FloatingButtonItem.js
@@ -9,7 +9,7 @@ import FloatingButtonItemLabel from './FloatingButtonItemLabel'
 
 class FloatingButtonItem extends Component {
   classes() {
-    const floatingButtonSize = 40;
+    const floatingButtonSize = 40
 
     return {
       'default': {
@@ -20,7 +20,7 @@ class FloatingButtonItem extends Component {
         },
         label: {
           position: 'absolute',
-          right: '100px',
+          right: '60px',
           top: '0',
           bottom: '0',
         },
@@ -33,24 +33,25 @@ class FloatingButtonItem extends Component {
           alignItems: 'center',
           justifyContent: 'center',
           boxShadow: '0px 4px 10px 0px rgba(221, 33, 48, 0.33)',
+          transition: 'box-shadow 0.2s cubic-bezier(0.4, 0, 0.2, 1)',
+          cursor: 'pointer',
         },
         buttonContent: {
           width: '24px',
         },
       },
       hover: {
-        floatingButton: {
-          boxShadow: '0 8px 17px 0 rgba(221, 33, 48, 0.28)',
+        button: {
+          boxShadow: '0 8px 17px 0 rgba(221, 33, 48, 0.7)',
         },
       },
       'size-large': {
         button: {
           height: floatingButtonSize * 1.3,
           width: floatingButtonSize * 1.3,
-          right: '900px',
         },
         label: {
-          right: '105px',
+          right: '67px', // TODO: make this a calculated distance
         },
       },
     }
@@ -62,6 +63,7 @@ class FloatingButtonItem extends Component {
         <div is="label">
           <FloatingButtonItemLabel
             label={ this.props.label }
+            hover={ this.props.hover }
           />
         </div>
       }
