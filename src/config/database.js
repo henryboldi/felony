554cc2eaab79d652d972aac103d3ec27f218f3d4
Henry Boldizsar <henrymb67@gmail.com>
2016-02-21 19:25:41 -0600

    Update database config

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,9 @@
 import low from 'lowdb'
+import storage from 'lowdb/file-async'
 import path from 'path'
 import os from 'os'
 
 const dbPath = path.join(os.homedir(), 'felony.json')
-const db = low(dbPath)
+const db = low(dbPath, { storage })
 
 export default db
