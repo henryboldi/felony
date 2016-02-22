8d78cf11278d3ae508ca50520e7f2fc6e051af60
Henry Boldizsar <henrymb67@gmail.com>
2016-02-22 15:23:52 -0600

    Update actions index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,2 @@
-import * as types from '../constants/ActionTypes'
-
-export function addKey(key) {
-  return { type: types.ADD_KEY, key }
-}
-
+export * from './KeyActions'
 export * from './UIActions'
