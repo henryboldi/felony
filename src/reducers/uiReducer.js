37503813e7b53796790c9d0f1e6e1a07e0b64412
Henry Boldizsar <henrymb67@gmail.com>
2016-02-22 15:24:42 -0600

    Move select key to uiReducer

--- src/reducers/uiReducer.js
+++ src/reducers/uiReducer.js
@@ -1,9 +1,10 @@
-import { TOGGLE_COMPOSE, SHOW_COMPOSE_WITH_TYPE } from '../constants/ActionTypes'
+import { TOGGLE_COMPOSE, SHOW_COMPOSE_WITH_TYPE, SELECT_KEY } from '../constants/UIConstants'
 import { OrderedMap } from 'immutable'
 
 const initialState = OrderedMap({
   expandedCompose: false,
   composeType: '',
+  activeKeys: OrderedMap({}),
 })
 
 const uiReducer = (state = initialState, action) => {
@@ -12,6 +13,13 @@ const uiReducer = (state = initialState, action) => {
       return state.set('expandedCompose', !state.get('expandedCompose'))
     case SHOW_COMPOSE_WITH_TYPE:
       return state.set('composeType', action.data).set('expandedCompose', true)
+    case SELECT_KEY:
+      if (state.hasIn(['activeKeys', action.id]) && state.getIn(['activeKeys', action.id]) === true) {
+        return state.setIn(['activeKeys', action.id], false)
+      } else {
+        return state.setIn(['activeKeys', action.id], true)
+      }
+
     default:
       return state
   }
