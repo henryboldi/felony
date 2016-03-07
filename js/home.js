6b135e28bc77cd1bfc7932ca33dad11bc9ed54e2
case <case@casesandberg.com>
2016-03-06 23:58:54 -0600

    Platforms + Styling

--- js/home.js
+++ js/home.js
@@ -3,18 +3,28 @@ $(function() {
   var canvas = document.querySelector('canvas');
   var cx = canvas.getContext('2d');
 
-  var width = $(window).width();
-  var height = width;
+  $(window).on('resize', (function() {
+    draw(cx);
+  }).bind(this));
 
-  cx.canvas.width = width;
-  cx.canvas.height = height;
+  function draw(cx) {
+    var width = $(window).width();
+    var height = $(window).height();
+    var extraHeight = 200;
 
-  cx.strokeStyle = '#D52035';
-  cx.beginPath();
-  cx.moveTo(40, -10);
-  cx.lineTo(width + 10, width - 100);
-  cx.lineWidth = 6;
-  cx.stroke();
-  cx.closePath();
+    cx.canvas.width = width;
+    cx.canvas.height = height + extraHeight;
+
+    cx.clearRect(0, 0, width, height);
+    cx.strokeStyle = '#D52035';
+    cx.beginPath();
+    cx.moveTo(width / 10, -10);
+    cx.lineTo(width + 10, height + extraHeight);
+    cx.lineWidth = 6;
+    cx.stroke();
+    cx.closePath();
+  }
+
+  draw(cx);
 
 });
