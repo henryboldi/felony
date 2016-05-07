352c08c0c58b2cd3fd8b7336e012a74d9f72e1bc
Henry Boldizsar <henrymb67@gmail.com>
2016-05-07 14:55:55 -0700

    Add alias composer form component

---
+++ app/components/alias/AliasComposerForm.js
@@ -0,0 +1,71 @@
+'use strict'
+
+import React, { Component } from 'react'
+import ReactCSS from 'reactcss'
+import dynamics from 'dynamics.js'
+
+import colors from '../../assets/styles/variables/colors'
+
+class AliasComposer extends Component {
+  classes() {
+    return {
+      'default': {
+        text: {
+          flex: '1',
+          position: 'relative',
+        },
+        textarea: {
+          Absolute: '23px 10px 10px 10px',
+          border: 'none',
+          outline: 'none',
+          width: '94%',
+          boxSizing: 'border-box',
+          padding: '10px 3%',
+          color: '#333',
+          borderRadius: '2px',
+          fontSize: '14px',
+          fontFamily: 'Andale Mono',
+        },
+        actions: {
+          marginTop: '-10px',
+          height: '40px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          padding: '0 10px',
+        },
+        link: {
+          textDecoration: 'none',
+          cursor: 'pointer',
+        },
+        cancel: {
+          color: '#999',
+        },
+        confirm: {
+          color: colors.primary,
+        },
+      },
+    }
+  }
+
+  handleCancel = () => {
+    this.props.toggleAliasComposer()
+  }
+
+  handleKeyDown = (e) => {
+    (e.keyCode === 27 && this.props.isShowingAliasComposer) && this.handleCancel()
+  }
+
+  render() {
+
+    return (
+      <div is="wrap" ref="wrap">
+        <div is="text">
+          <textarea is="textarea" ref="textarea" placeholder={ props.placeholder } onKeyDown={ this.handleKeyDown } />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ReactCSS(AliasComposer)
