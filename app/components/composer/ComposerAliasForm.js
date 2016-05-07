9492ddc1d4177c8ea7012e3b97bb2459f301ca37
Henry Boldizsar <henrymb67@gmail.com>
2016-05-07 14:56:28 -0700

    Add composer alias form component

---
+++ app/components/composer/ComposerAliasForm.js
@@ -0,0 +1,124 @@
+'use strict'
+
+import React, { Component } from 'react'
+import ReactCSS from 'reactcss'
+import dynamics from 'dynamics.js'
+import { generateKey } from '../../utils/pgp'
+
+import colors from '../../assets/styles/variables/colors'
+
+class ComposerAliasForm extends Component {
+  classes() {
+    return {
+      'default': {
+        wrap: {
+          position: 'fixed',
+          top: '0',
+          right: '0',
+          bottom: '0',
+          left: '0',
+          zIndex: '13',
+          flexDirection: 'column',
+          display: 'flex',
+        },
+        text: {
+          flex: '1',
+          position: 'relative',
+        },
+        formItem: {
+          display: 'block',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          width: '94%',
+        },
+        input: {
+          border: 'none',
+          outline: 'none',
+          width: '100%',
+          boxSizing: 'border-box',
+          padding: '10px 3%',
+          color: '#333',
+          borderRadius: '2px',
+          fontSize: '14px',
+          fontFamily: 'Andale Mono',
+        },
+        actions: {
+          marginTop: '-10px',
+          height: '40px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          padding: '0 10px',
+        },
+        link: {
+          textDecoration: 'none',
+          cursor: 'pointer',
+        },
+        cancel: {
+          color: '#999',
+        },
+        confirm: {
+          color: colors.primary,
+        },
+      },
+    }
+  }
+
+  handleConfirm = async () => {
+    const name = this.refs.form[0].value
+    const email = this.refs.form[1].value
+    const passphrase = this.refs.form[2].value
+
+    console.log(name, email, passphrase)
+
+    const key = await generateKey({ name, email }, passphrase)
+    console.log(key)
+
+    await this.props.addKey(key)
+    console.log('added key!')
+  }
+
+  render() {
+    return (
+      <div is="wrap" ref="wrap">
+        <form is="form" ref="form">
+          <div is="formItem">
+            <label>Name</label>
+            <input
+              is="input"
+              ref="textarea"
+              placeholder={ 'Name' }
+              onKeyDown={ this.props.handleKeyDown }
+            />
+          </div>
+          <div is="formItem">
+            <label>Email</label>
+            <input
+              type="email"
+              is="input"
+              ref="textarea"
+              placeholder={ 'Email' }
+              onKeyDown={ this.props.handleKeyDown }
+            />
+          </div>
+          <div is="formItem">
+            <label>Passphrase</label>
+            <input
+              type="password"
+              is="input"
+              ref="textarea"
+              placeholder={ 'Passphrase' }
+              onKeyDown={ this.props.handleKeyDown }
+            />
+          </div>
+        </form>
+        <div is="actions">
+          <a is="link cancel" onClick={ this.props.handleCancel }>Cancel</a>
+          <a is="link confirm" onClick={ this.handleConfirm }>Generate Key Pair</a>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ReactCSS(ComposerAliasForm)
