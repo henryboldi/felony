5920790c0b460eb594eae0bc9e8533303740a344
Henry Boldizsar <henrymb67@gmail.com>
2016-05-07 14:51:27 -0700

    Add alias actions

---
+++ app/actions/AliasesActions.js
@@ -0,0 +1,32 @@
+import * as types from '../constants/AliasesConstants'
+import db from '../config/database.js'
+import { generateKey } from '../../utils/pgp'
+
+import uuid from 'uuid'
+
+export function addAlias(alias) {
+  return async function (dispatch) {
+    try {
+      const generatedKey = await generateKey()
+      const insertedAlias = await db('aliases').insert(alias)
+      return dispatch({ type: types.ADD_KEY, alias: insertedAlias })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
+
+export function setAliases(aliases) {
+  return { type: types.SET_KEYCHAIN, aliases }
+}
+
+export function fetchAliases() {
+  return async function (dispatch) {
+    try {
+      const aliases = await db('aliases').value()
+      return dispatch(setAliases(aliases))
+    } catch (err) {
+
+    }
+  }
+}
