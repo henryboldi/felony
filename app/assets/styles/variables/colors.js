850d86648cd723ccd07f72975d115fe9de22bdb0
Henry Boldizsar <henrymb67@gmail.com>
2016-06-07 17:27:05 -0700

    Add new colors

--- app/assets/styles/variables/colors.js
+++ app/assets/styles/variables/colors.js
@@ -1,9 +1,11 @@
 const colors = {
   bgDark: '#2B272C',
   bgLight: '#37373C',
+  bgGrey: '#EAE4E4',
   offBgLight: '#DEE3ED',
   primary: '#E4212A',
   primaryGradient: 'linear-gradient(to left bottom, #E4212A 0%, #B61E4F 100%)',
+  green: '#00E676',
 }
 
 export default colors
