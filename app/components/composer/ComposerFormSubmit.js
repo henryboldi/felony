89a3a3fcdd1cb17fae5e784534bcf0b3da60e2a2
Henry Boldizsar <henrymb67@gmail.com>
2016-06-09 15:53:46 -0700

    Add composer form submit

---
+++ app/components/composer/ComposerFormSubmit.js
@@ -0,0 +1,39 @@
+'use strict'
+
+import React, { Component } from 'react'
+import ReactCSS from 'reactcss'
+
+import colors from '../../assets/styles/variables/colors'
+
+class ComposerFormSubmit extends Component {
+  classes() {
+    return {
+      'default': {
+        confirm: {
+          color: '#fff',
+          padding: '10px 30px',
+          borderRadius: '5px',
+          backgroundColor: colors.primary,
+          textDecoration: 'none',
+          cursor: 'pointer',
+          textAlign: 'center',
+          display: 'block',
+          transition: '200ms ease-in-out',
+        },
+      },
+      hover: {
+        confirm: {
+          backgroundColor: '#BF1B23',
+        },
+      },
+    }
+  }
+
+  render() {
+    return (
+      <a is="link confirm" onClick={ this.props.onClick }>{ this.props.value }</a>
+    )
+  }
+}
+
+export default ReactCSS.Hover(ReactCSS(ComposerFormSubmit))
