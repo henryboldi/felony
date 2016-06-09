4f92b5b4eb89a9fdaa89961e860bb3afa5ed4347
Henry Boldizsar <henrymb67@gmail.com>
2016-06-09 15:56:29 -0700

    Add header key status spinner

---
+++ app/components/header/HeaderKeyStatusSpinner.js
@@ -0,0 +1,19 @@
+'use strict'
+
+import React, { Component } from 'react'
+
+import colors from '../../assets/styles/variables/colors'
+
+class HeaderKeyStatusSpinner extends Component {
+
+  render() {
+    return <div>
+      <div className="spinner">
+        <div className="double-bounce1"></div>
+        <div className="double-bounce2"></div>
+      </div>
+    </div>
+  }
+}
+
+export default HeaderKeyStatusSpinner
