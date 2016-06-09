6b99191897a71e73df4332979515730afd3f424a
Henry Boldizsar <henrymb67@gmail.com>
2016-06-09 15:56:20 -0700

    Add header key status

---
+++ app/components/header/HeaderKeyStatus.js
@@ -0,0 +1,67 @@
+'use strict'
+
+import React, { Component } from 'react'
+import ReactCSS from 'reactcss'
+
+import HeaderKeyCopy from './HeaderKeyCopy'
+import HeaderKeyStatusSpinner from './HeaderKeyStatusSpinner'
+import HeaderKeyStatusTooltip from './HeaderKeyStatusTooltip'
+
+class HeaderKeyStatus extends Component {
+  state = { isShowingTooltip: false }
+
+  classes() {
+    return {
+      'default': {
+
+      },
+    }
+  }
+
+  setTooltip = (b) => {
+    this.setState({ isShowingTooltip: b })
+  }
+
+  handleMouseEnter = () => {
+    this.setTooltip(true)
+  }
+
+  handleMouseLeave = () => {
+    this.setTooltip(false)
+  }
+
+  render() {
+    return <div>
+      <div
+        onMouseEnter={ this.handleMouseEnter }
+        onMouseLeave={ this.handleMouseLeave }
+      >
+        { this.props.isGeneratingKey ?
+          <div>
+            <HeaderKeyStatusSpinner />
+          </div>
+        :
+          <div>
+            <HeaderKeyCopy
+              alias={ this.props.alias }
+              isCopied={ this.props.isCopied }
+              toggleIsCopied={ this.props.toggleIsCopied }
+            />
+          </div>
+        }
+        { this.state.isShowingTooltip ?
+          <div>
+            <HeaderKeyStatusTooltip
+              isGeneratingKey={ this.props.isGeneratingKey }
+              isCopied={ this.props.isCopied }
+            />
+          </div>
+        :
+          null
+        }
+      </div>
+    </div>
+  }
+}
+
+export default ReactCSS(HeaderKeyStatus)
