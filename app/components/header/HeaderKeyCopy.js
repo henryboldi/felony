d08c4cfac20368e4f4c8f9a1590dd8cecb575b99
Henry Boldizsar <henrymb67@gmail.com>
2016-06-09 15:56:11 -0700

    Add header key copy

---
+++ app/components/header/HeaderKeyCopy.js
@@ -0,0 +1,54 @@
+'use strict'
+
+import React, { Component } from 'react'
+import ReactCSS from 'reactcss'
+import CopyToClipboard from 'react-copy-to-clipboard'
+
+import { Icon } from '../common/index'
+
+import colors from '../../assets/styles/variables/colors'
+
+class HeaderKeyCopy extends Component {
+
+  classes() {
+    return {
+      'default': {
+        copy: {
+          cursor: 'pointer',
+          alignSelf: 'flex-end',
+          marginTop: '5px',
+        },
+      },
+    }
+  }
+
+  render() {
+    return <div is="copy">
+      <CopyToClipboard
+        text={ this.props.alias.publicKeyArmored }
+        onCopy={() => {
+          this.props.toggleIsCopied()
+          setTimeout(() => {
+            this.props.toggleIsCopied()
+          }, 2000)
+        } }
+      >
+        { this.props.isCopied ?
+          <Icon
+            name="check"
+            color={ colors.green }
+          />
+        :
+          <a>
+            <Icon
+              name="copy"
+              color={ colors.primary }
+            />
+          </a>
+        }
+      </CopyToClipboard>
+    </div>
+  }
+}
+
+export default ReactCSS(HeaderKeyCopy)
