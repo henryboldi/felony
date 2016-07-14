390a73f5270690c7255677999e8c2352acea2793
Gregory Scheerlinck <gregory.scheerlinck@gmail.com>
2016-07-14 03:24:10 +0100

    fixed (hopefully) more linting errors and warnings

--- main.development.js
+++ main.development.js
@@ -1,4 +1,5 @@
 import { app, BrowserWindow, Menu, shell, autoUpdater } from 'electron'
+import electronDebug from 'electron-debug'
 const { version } = require('./package.json')
 import open from 'open'
 
@@ -6,7 +7,7 @@ let menu
 let template
 let mainWindow = null
 
-const feedUrl = `https://felony-app-update.herokuapp.com/update/${process.platform}_${process.arch}/${version}`
+const feedUrl = `https://felony-app-update.herokuapp.com/update/${ process.platform }_${ process.arch }/${ version }`
 
 if (process.env.NODE_ENV !== 'development') {
   autoUpdater.setFeedURL(feedUrl)
