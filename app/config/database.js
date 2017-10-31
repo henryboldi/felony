66c49136cc6fd31e05feaa414e9869835613bcce
rehanj <33221769+rehanj@users.noreply.github.com>
2017-10-31 23:06:49 +0530

    Update.database.js

--- app/config/database.js
+++ app/config/database.js
@@ -3,7 +3,7 @@ import storage from 'lowdb/file-async'
 import path from 'path'
 import os from 'os'
 import underscoreDB from 'underscore-db'
-
+//felony
 const dbPath = path.join(os.homedir(), 'felony.json')
 const db = low(dbPath, { storage })
 
