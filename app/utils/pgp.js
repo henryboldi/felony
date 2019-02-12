43ce0e0905af7b97b9af560ac7ac29dff91597cc
Henry Boldizsar <henrymb67@gmail.com>
2019-02-12 13:16:56 -0600

    Update pgp.js

--- app/utils/pgp.js
+++ app/utils/pgp.js
@@ -13,7 +13,7 @@ function applyFelonyBranding(openpgpBrandedKey) {
   let output
   output = openpgpBrandedKey.replace(/Version: OpenPGP\.js [^<]\d\.\d\.\d/g,
     'Version: 🔑 Felony (PGP made easy) v0.0.1')
-  output = output.replace('Comment: http://openpgpjs.org', 'Comment: 👀 How do I use this? --> http://felony.io 😉')
+  output = output.replace('Comment: http://openpgpjs.org', 'Comment: How do I use this? https://github.com/henryboldi/felony')
   return output
 }
 
